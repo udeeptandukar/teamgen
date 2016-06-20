a3b8c144272f17eaecee753e032268a7f86c70ee
udp-np <udeeptandukar@gmail.com>
2016-06-19 21:23:56 -0600

    Fix create/replace entity

--- member.go
+++ member.go
@@ -10,20 +10,26 @@ import (
 func addMember(ctx context.Context, teamID string, channelID string, members []string) slashResponse {
 	key := generateTeamGeneratorKey(ctx, teamID, channelID)
 
-	var teamGenerator TeamGenerator
-	if err := datastore.Get(ctx, key, &teamGenerator); err != nil {
-		teamGenerator = TeamGenerator{
-			SlackTeamID:    teamID,
-			SlackChannelID: channelID,
-			Members:        members,
-			LastUpdated:    time.Now(),
-		}
-	} else {
-		teamGenerator.Members = members
-		teamGenerator.LastUpdated = time.Now()
+	// var teamGenerator TeamGenerator
+	// if err := datastore.Get(ctx, key, &teamGenerator); err != nil {
+	// 	teamGenerator := &TeamGenerator{
+	// 		SlackTeamID:    teamID,
+	// 		SlackChannelID: channelID,
+	// 		Members:        members,
+	// 		LastUpdated:    time.Now(),
+	// 	}
+	// } else {
+	// 	teamGenerator.Members = members
+	// 	teamGenerator.LastUpdated = time.Now()
+	// }
+	teamGenerator := &TeamGenerator{
+		SlackTeamID:    teamID,
+		SlackChannelID: channelID,
+		Members:        members,
+		LastUpdated:    time.Now(),
 	}
 
-	if _, err := datastore.Put(ctx, key, teamGenerator); err != nil {
+	if _, err := datastore.Put(ctx, key, teamGenerator); err == nil {
 		return constructSlashResponse("ephemeral", "Team members added")
 	}
 
