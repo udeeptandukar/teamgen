1304231a32934d1133842aacdc283bb370678ea6
udp-np <udeeptandukar@gmail.com>
2016-06-25 20:52:24 -0600

    Add slack specific configs

--- config.go
+++ config.go
@@ -1,5 +1,8 @@
 package teamgen
 
 var (
-	cmdToken = "4TWKP2dQGXC7or1gK868SKuj"
+	clientID          = "52127528597.53511047264"
+	clientSecret      = "a88966fde9bafbca5d7e212cf0f84c41"
+	verificationToken = "KhiPIXahhJM8SI8eGFytnZ2l"
+	oauthAccessURL    = "https://slack.com/api/oauth.access"
 )
