b5656d3172d13527241b156466bcc8cfd41b657a
udp-np <udeeptandukar@gmail.com>
2017-01-25 20:32:50 -0600

    Support auth and no auth message in home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'home',
@@ -6,4 +7,22 @@ import {Component} from '@angular/core';
   templateUrl: './home.component.html'
 })
 export class HomeComponent {
+  public status:string = '';
+  public sub;
+  
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router) {}
+
+  ngOnInit() {
+    this.sub = this.route
+      .queryParams
+      .subscribe(params => {
+        this.status = params['status'] || '';
+      });
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
 }
