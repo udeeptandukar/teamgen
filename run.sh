78c928d83f8710f7e545322ea86eb297ac22ad0b
udp-np <udeeptandukar@gmail.com>
2017-01-29 11:20:41 -0600

    Update app.yaml and run.sh - Include node_modules, src/app in skip_files section of app.yaml - Update run.sh to deploy test env and custom versions

--- run.sh
+++ run.sh
@@ -3,8 +3,10 @@
 #This script is used to run deployment, test of this go app
 
 CMD="$1"
+APP="$2"
+VERSION="$3"
 if [ $CMD == deploy ]; then
-    $HOME/go_appengine/appcfg.py update "$PWD"
+    goapp deploy -application "$APP" -version "$VERSION" app.yaml
 elif [ $CMD == test ]; then
     go test
 fi
